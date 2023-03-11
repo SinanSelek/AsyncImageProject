dc7c23e293447b218ab08a31f00fb742aad0413c
Sinan Selek <sselek62@gmail.com>
2023-03-11 12:43:44 +0300

    Initial Commit

---
+++ AsyncImageProject/AsyncImageProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageProjectApp.swift
+//  AsyncImageProject
+//
+//  Created by Sinan Selek on 11.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
